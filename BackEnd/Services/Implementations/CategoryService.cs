caea4d31fa6072489fdd298d4c3cea3fe2e9e293
DanielF4918 <145735294+DanielF4918@users.noreply.github.com>
2025-01-27 20:03:14 -0600

    CategoryService

---
+++ BackEnd/Services/Implementations/CategoryService.cs
@@ -0,0 +1,37 @@
+﻿using BackEnd.Services.Interfaces;
+using DAL.Interfaces;
+using Entities.Entities;
+
+namespace BackEnd.Services.Implementations
+{
+    public class CategoryService : ICategoryService
+    {
+
+        private ICategoryDAL _categoryDAL;
+
+        public CategoryService(ICategoryDAL categoryDAL)
+        {
+            _categoryDAL = categoryDAL;
+        }
+
+        public void AddCategory(Category category)
+        {
+            _categoryDAL.AddCategory(category);
+        }
+
+        public void DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BackEnd/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Entities;
+
+namespace BackEnd.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+
+        void AddCategory(Category category);
+        void UpdateCategory(Category category);
+        void DeleteCategory(int id);
+        List<Category> GetAllCategories();
+    }
+}
