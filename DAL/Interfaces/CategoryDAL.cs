dbabf1ec2e569c9dfb56812f99766742022cb610
DanielF4918 <145735294+DanielF4918@users.noreply.github.com>
2025-01-27 19:56:39 -0600

    Add sin GR

---
+++ DAL/Interfaces/CategoryDAL.cs
@@ -0,0 +1,47 @@
+﻿using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public class CategoryDAL : ICategoryDAL
+    {
+        private NorthwndContext _context;
+
+        public CategoryDAL(NorthwndContext context) 
+        { 
+           _context = context;
+        }
+        public bool AddCategory(Category category)
+        {
+            try
+            {
+                _context.Add(category);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
